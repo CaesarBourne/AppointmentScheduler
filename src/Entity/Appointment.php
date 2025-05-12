a71243745b983d67e46c95ed5fe289a1458f1140
“CaesarBourne” <caesaradek@gmail.com>
2025-05-12 22:31:45 +0100

    created apps root data with doctrine for database appointment schedulker and added packages

---
+++ src/Entity/Appointment.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AppointmentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AppointmentRepository::class)]
+class Appointment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTime $startTime = null;
+
+    #[ORM\Column]
+    private ?\DateTime $endTime = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $participant = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStartTime(): ?\DateTime
+    {
+        return $this->startTime;
+    }
+
+    public function setStartTime(\DateTime $startTime): static
+    {
+        $this->startTime = $startTime;
+
+        return $this;
+    }
+
+    public function getEndTime(): ?\DateTime
+    {
+        return $this->endTime;
+    }
+
+    public function setEndTime(\DateTime $endTime): static
+    {
+        $this->endTime = $endTime;
+
+        return $this;
+    }
+
+    public function getParticipant(): ?string
+    {
+        return $this->participant;
+    }
+
+    public function setParticipant(string $participant): static
+    {
+        $this->participant = $participant;
+
+        return $this;
+    }
+}
